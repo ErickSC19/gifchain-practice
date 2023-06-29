7efa7b8e75692f581f0054db89f899469c5b024e
Erick <adolfo.solis.is@unipolidgo.edu.mx>
2023-06-29 01:09:31 -0600

    structure

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
